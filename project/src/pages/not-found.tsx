3e0cd6ff0f9702bb4a014a0e9a51e3044eece097
Tamara <tamara.lavrina@gmail.com>
2022-02-27 10:55:41 +0300

    добавляет стили

--- project/src/pages/not-found.tsx
+++ project/src/pages/not-found.tsx
@@ -1,15 +1,18 @@
 import Footer from '../components/footer/footer';
-import HeaderSignIn from '../components/header/header';
+import HeaderSignIn from '../components/header-sign-in/header-sign-in';
 import {Link} from 'react-router-dom';
+import styles from './not-found.module.css';
+
 
 function NotFound(): JSX.Element {
   return (
-    <div className='user-page'>
+
+    <div className={`user-page ${styles.body}`}>
       <HeaderSignIn />
       <section className='catalog'>
-        <div className="page-content">
-          <h1>404. Page not found</h1>
-          <Link to="/">Вернуться на главную</Link>
+        <div className={styles.content}>
+          <h1 className={styles.h1}>404. Page not found</h1>
+          <Link to="/" className={styles.goHome}>Вернуться на главную</Link>
         </div>
       </section>
 
