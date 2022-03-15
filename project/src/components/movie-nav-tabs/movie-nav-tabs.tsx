658218dd052ab0af7817e13c4a69c38685694e3b
Tamara <tamara.lavrina@gmail.com>
2022-03-15 17:34:36 +0300

    правки по табам

--- project/src/components/movie-nav-tabs/movie-nav-tabs.tsx
+++ project/src/components/movie-nav-tabs/movie-nav-tabs.tsx
@@ -1,59 +1,60 @@
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import { CardType } from '../../mocks/mocks';
+import { CardType, TABS } from '../../mocks/mocks';
 import OverviewTab from '../movie-tabs/movie-overview-tab';
 import DetailsTab from '../movie-tabs/movie-details-tab';
 import ReviewTab from '../movie-tabs/movie-review-tab';
+import cn from 'classnames';
 
 type MovieNavTabsProps = {
   movie: CardType;
 };
 
-type MovieTabItemProps = {
-  title: string;
-};
-
 function MovieNavTabs({ movie }: MovieNavTabsProps): JSX.Element {
-  const movieTabs: MovieTabItemProps[] = [
-    {
-      title: 'Overview',
-    },
-    {
-      title: 'Details',
-    },
-    {
-      title: 'Reviews',
-    },
-  ];
+  const [activeTab, setActiveTab] = useState<string>(TABS[0]);
 
-  const [activeTab, setActiveTab] = useState<string>('Overview');
+  const handleClick = (tab: string) => {
+    setActiveTab(tab);
+  };
 
-  const handleClick = (title: string) => {
-    setActiveTab(title);
+  const renderTab = () => {
+    switch (activeTab) {
+      case 'Overview':
+        return <OverviewTab movie={movie} />;
+      case 'Details':
+        return <DetailsTab movie={movie} />;
+      case 'Reviews':
+        return <ReviewTab movie={movie} />;
+      default:
+        throw new Error(`Unknown tab type ${activeTab}`);
+    }
   };
 
   return (
     <>
       <nav className="film-nav film-card__nav">
         <ul className="film-nav__list">
-          {movieTabs.map((tab) => (
+          {TABS.map((tab: string) => (
             <li
-              key={tab.title}
-              className={`film-nav__item ${
-                activeTab === tab.title ? 'film-nav__item--active' : ''
-              }`}
-              onClick={() => handleClick(tab.title)}
+              key={tab}
+              className={cn('film-nav__item', {
+                'film-nav__item--active': activeTab === tab,
+              })}
             >
-              <Link to={'#'} className="film-nav__link">
-                {tab.title}
-              </Link>
+              <a
+                href="/#"
+                className="film-nav__link"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  handleClick(tab);
+                }}
+              >
+                {tab}
+              </a>
             </li>
           ))}
         </ul>
       </nav>
-      {activeTab === 'Overview' && <OverviewTab movie={movie} />}
-      {activeTab === 'Details' && <DetailsTab movie={movie} />}
-      {activeTab === 'Reviews' && <ReviewTab movie={movie} />}
+      {renderTab()}
     </>
   );
 }
--- project/src/mocks/mocks.tsx
+++ project/src/mocks/mocks.tsx
@@ -1,104 +1,3 @@
-// const filmsList = [
-//   {
-//     title: 'Fantastic Beasts: The Crimes of Grindelwald',
-//     previewImg: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
-//   },
-
-//   {
-//     title: 'Bohemian Rhapsody',
-//     previewImg: 'img/bohemian-rhapsody.jpg',
-//   },
-
-//   {
-//     title: 'Macbeth',
-//     previewImg: 'img/macbeth.jpg',
-//   },
-
-//   {
-//     title: 'Aviator',
-//     previewImg: 'img/aviator.jpg',
-//   },
-//   {
-//     title: 'We need to talk about Kevin',
-//     previewImg: 'img/we-need-to-talk-about-kevin.jpg',
-//   },
-
-//   {
-//     title: 'What We Do in the Shadows',
-//     previewImg: 'img/what-we-do-in-the-shadows.jpg',
-//   },
-
-//   {
-//     title: 'Revenant',
-//     previewImg: 'img/revenant.jpg',
-//   },
-
-//   {
-//     title: 'Johnny English',
-//     previewImg: 'img/johnny-english.jpg',
-//   },
-
-//   {
-//     title: 'Shutter Island',
-//     previewImg: 'img/shutter-island.jpg',
-//   },
-
-//   {
-//     title: 'Pulp Fiction',
-//     previewImg: 'img/pulp-fiction.jpg',
-//   },
-
-//   {
-//     title: 'No Country for Old Men',
-//     previewImg: 'img/no-country-for-old-men.jpg',
-//   },
-
-//   {
-//     title: 'Snatch',
-//     previewImg: 'img/snatch.jpg',
-//   },
-
-//   {
-//     title: 'Moonrise Kingdom',
-//     previewImg: 'img/moonrise-kingdom.jpg',
-//   },
-
-//   {
-//     title: 'Seven Years in Tibet',
-//     previewImg: 'img/seven-years-in-tibet.jpg',
-//   },
-
-//   {
-//     title: 'Midnight Special',
-//     previewImg: 'img/midnight-special.jpg',
-//   },
-
-//   {
-//     title: 'War of the Worlds',
-//     previewImg: 'img/war-of-the-worlds.jpg',
-//   },
-
-//   {
-//     title: 'Dardjeeling Limited',
-//     previewImg: 'img/dardjeeling-limited.jpg',
-//   },
-
-//   {
-//     title: 'Orlando',
-//     previewImg: 'img/orlando.jpg',
-//   },
-
-//   {
-//     title: 'Mindhunter',
-//     previewImg: 'img/mindhunter.jpg',
-//   },
-
-//   {
-//     title: 'Midnight Special',
-//     previewImg: 'img/midnight-special.jpg',
-//   },
-// ];
-
 enum Genres {
   'All genres',
   'Comedies',
@@ -108,9 +7,8 @@ enum Genres {
   'Kids & Family',
   'Romance',
   'Sci-Fi',
-  'Thrillers'
+  'Thrillers',
 }
-
 
 const PromoBudapest = {
   poster: 'the-grand-budapest-hotel-poster.jpg',
@@ -120,58 +18,60 @@ const PromoBudapest = {
 };
 
 export type PromoCardType = {
-  id: number,
-  title: string,
-}
+  id: number;
+  title: string;
+};
 
 export type CardType = {
-  backgroundColor: string,
-  backgroundImg: string
-  description: string,
-  director: string,
-  duration: number,
-  genre: string,
-  id: number,
-  isInMyList: boolean,
-  movieLink: string,
-  poster: string,
-  previewImg: string,
-  previewVideoLink: string,
-  rating: number,
-  releaseDate: number,
-  scores: number,
-  starring: string[],
-  title: string,
-}
+  backgroundColor: string;
+  backgroundImg: string;
+  description: string;
+  director: string;
+  duration: number;
+  genre: string;
+  id: number;
+  isInMyList: boolean;
+  movieLink: string;
+  poster: string;
+  previewImg: string;
+  previewVideoLink: string;
+  rating: number;
+  releaseDate: number;
+  scores: number;
+  starring: string[];
+  title: string;
+};
 
 const films: CardType[] = [
   {
     backgroundColor: '#f0dba2',
-    backgroundImg: 'https://9.react.pages.academy/static/film/background/Johnny_English.jpg',
-    description: 'After a sudden attack on the MI5, Johnny English, Britain\'s most confident yet unintelligent spy, becomes Britain\'s only spy.',
+    backgroundImg:
+      'https://9.react.pages.academy/static/film/background/Johnny_English.jpg',
+    description:
+      'After a sudden attack on the MI5, Johnny English, Britain\'s most confident yet unintelligent spy, becomes Britain\'s only spy.',
     director: 'Peter Howitt',
     duration: 88,
     genre: 'Comedy',
     id: 1,
     isInMyList: false,
     movieLink: 'https://9.react.pages.academy/static/film/video/bubbles.mp4',
-    poster: 'https://9.react.pages.academy/static/film/poster/Johnny_English.jpg',
-    previewImg: 'https://9.react.pages.academy/static/film/preview/johnny-english.jpg',
+    poster:
+      'https://9.react.pages.academy/static/film/poster/Johnny_English.jpg',
+    previewImg:
+      'https://9.react.pages.academy/static/film/preview/johnny-english.jpg',
     previewVideoLink: 'https://9.react.pages.academy/static/film/video/dog.mp4',
     rating: 10,
     releaseDate: 2003,
     scores: 136843,
-    starring: [
-      'Rowan Atkinson',
-      'John Malkovich',
-      'Natalie Imbruglia',
-    ],
+    starring: ['Rowan Atkinson', 'John Malkovich', 'Natalie Imbruglia'],
     title: 'Johnny English',
   },
   {
     backgroundColor: '#e1dad7',
-    backgroundImg: 'https://9.react.pages.academy/static/film/background/legend.jpg',
-    description: 'Identical twin gangsters Ronald and Reginald Kray terrorize London during the 1960s.',
+    backgroundImg:
+      'https://9.react.pages.academy/static/film/background/legend.jpg',
+    description:
+      'Identical twin gangsters Ronald and Reginald Kray terrorize London during the 1960s.',
     director: 'Brian Helgeland',
     duration: 132,
     genre: 'Crime',
@@ -184,86 +84,86 @@ const films: CardType[] = [
     rating: 3.5,
     releaseDate: 2015,
     scores: 138487,
-    starring: [
-      'Tom Hardy',
-      'Emily Browning',
-      'Taron Egerton',
-    ],
+    starring: ['Tom Hardy', 'Emily Browning', 'Taron Egerton'],
     title: 'Legend',
   },
   {
     backgroundColor: '#a39e81',
-    backgroundImg: 'https://9.react.pages.academy/static/film/background/What-We-Do-in-the-Shadows.jpg',
-    description: 'A look into the daily (or rather, nightly) lives of three vampires who\'ve lived together for over 100 years, in Staten Island.',
+    backgroundImg:
+      'https://9.react.pages.academy/static/film/background/What-We-Do-in-the-Shadows.jpg',
+    description:
+      'A look into the daily (or rather, nightly) lives of three vampires who\'ve lived together for over 100 years, in Staten Island.',
     director: 'Jemaine Clement',
     duration: 30,
     genre: 'Comedy',
     id: 3,
     isInMyList: false,
     movieLink: 'https://9.react.pages.academy/static/film/video/bike.mp4',
-    poster: 'https://9.react.pages.academy/static/film/poster/What-We-Do-in-the-Shadows.jpg',
-    previewImg: 'https://9.react.pages.academy/static/film/preview/what-we-do-in-the-shadows.jpg',
-    previewVideoLink: 'https://9.react.pages.academy/static/film/video/traffic.mp4',
+    poster:
+      'https://9.react.pages.academy/static/film/poster/What-We-Do-in-the-Shadows.jpg',
+    previewImg:
+      'https://9.react.pages.academy/static/film/preview/what-we-do-in-the-shadows.jpg',
+    previewVideoLink:
+      'https://9.react.pages.academy/static/film/video/traffic.mp4',
     rating: 7.2,
     releaseDate: 2019,
     scores: 6173,
-    starring: [
-      'Kayvan Novak',
-      'Matt Berry',
-      'Natasia Demetriou',
-    ],
+    starring: ['Kayvan Novak', 'Matt Berry', 'Natasia Demetriou'],
     title: 'What We Do in the Shadows',
   },
   {
     backgroundColor: '#ad9f8b',
-    backgroundImg: 'https://9.react.pages.academy/static/film/background/Dardjeeling_Limited.jpg',
-    description: 'A year after their father\'s funeral, three brothers travel across India by train in an attempt to bond with each other.',
+    backgroundImg:
+      'https://9.react.pages.academy/static/film/background/Dardjeeling_Limited.jpg',
+    description:
+      'A year after their father\'s funeral, three brothers travel across India by train in an attempt to bond with each other.',
     director: 'Wes Anderson',
     duration: 91,
     genre: 'Adventure',
     id: 4,
     isInMyList: false,
     movieLink: 'https://9.react.pages.academy/static/film/video/matrix.mp4',
-    poster: 'https://9.react.pages.academy/static/film/poster/Dardjeeling_Limited.jpg',
-    previewImg: 'https://9.react.pages.academy/static/film/preview/dardjeeling_limited.jpg',
+    poster:
+      'https://9.react.pages.academy/static/film/poster/Dardjeeling_Limited.jpg',
+    previewImg:
+      'https://9.react.pages.academy/static/film/preview/dardjeeling_limited.jpg',
     previewVideoLink: 'https://9.react.pages.academy/static/film/video/dog.mp4',
     rating: 3.6,
     releaseDate: 2007,
     scores: 165106,
-    starring: [
-      'Owen Wilson',
-      'Adrien Brody',
-      'Jason Schwartzman',
-    ],
+    starring: ['Owen Wilson', 'Adrien Brody', 'Jason Schwartzman'],
     title: 'Dardjeeling Limited',
   },
   {
     backgroundColor: '#e1dfde',
-    backgroundImg: 'https://9.react.pages.academy/static/film/background/We_need_to_talk_about_Kevin.jpg',
-    description: 'Kevin\'s mother struggles to love her strange child, despite the increasingly dangerous things he says and does as he grows up. But Kevin is just getting started, and his final act will be beyond anything anyone imagined.',
+    backgroundImg:
+      'https://9.react.pages.academy/static/film/background/We_need_to_talk_about_Kevin.jpg',
+    description:
+      'Kevin\'s mother struggles to love her strange child, despite the increasingly dangerous things he says and does as he grows up. But Kevin is just getting started, and his final act will be beyond anything anyone imagined.',
     director: 'Lynne Ramsay',
     duration: 112,
     genre: 'Drama',
     id: 5,
     isInMyList: false,
     movieLink: 'https://9.react.pages.academy/static/film/video/matrix.mp4',
-    poster: 'https://9.react.pages.academy/static/film/poster/We_need_to_talk_about_Kevin.jpg',
-    previewImg: 'https://9.react.pages.academy/static/film/preview/we-need-to-talk-about-kevin.jpg',
-    previewVideoLink: 'https://9.react.pages.academy/static/film/video/traffic.mp4',
+    poster:
+      'https://9.react.pages.academy/static/film/poster/We_need_to_talk_about_Kevin.jpg',
+    previewImg:
+      'https://9.react.pages.academy/static/film/preview/we-need-to-talk-about-kevin.jpg',
+    previewVideoLink:
+      'https://9.react.pages.academy/static/film/video/traffic.mp4',
     rating: 3.8,
     releaseDate: 2011,
     scores: 123240,
-    starring: [
-      'Tilda Swinton',
-      'John C. Reilly',
-      'Ezra Miller',
-    ],
+    starring: ['Tilda Swinton', 'John C. Reilly', 'Ezra Miller'],
     title: 'We need to talk about Kevin',
   },
   {
     backgroundColor: '#f1e9ce',
-    backgroundImg: 'https://9.react.pages.academy/static/film/background/Macbeth.jpg',
-    description: 'Macbeth, the Thane of Glamis, receives a prophecy from a trio of witches that one day he will become King of Scotland. Consumed by ambition and spurred to action by his wife, Macbeth murders his king and takes the throne for himself.',
+    backgroundImg:
+      'https://9.react.pages.academy/static/film/background/Macbeth.jpg',
+    description:
+      'Macbeth, the Thane of Glamis, receives a prophecy from a trio of witches that one day he will become King of Scotland. Consumed by ambition and spurred to action by his wife, Macbeth murders his king and takes the throne for himself.',
     director: 'Justin Kurzel',
     duration: 113,
     genre: 'Drama',
@@ -272,21 +172,20 @@ const films: CardType[] = [
     movieLink: 'https://9.react.pages.academy/static/film/video/bubbles.mp4',
     poster: 'https://9.react.pages.academy/static/film/poster/Macbeth.jpg',
     previewImg: 'https://9.react.pages.academy/static/film/preview/macbeth.jpg',
-    previewVideoLink: 'https://9.react.pages.academy/static/film/video/traffic.mp4',
+    previewVideoLink:
+      'https://9.react.pages.academy/static/film/video/traffic.mp4',
     rating: 3.3,
     releaseDate: 2015,
     scores: 48798,
-    starring: [
-      'Michael Fassbender',
-      'Marion Cotillard',
-      'Jack Madigan',
-    ],
+    starring: ['Michael Fassbender', 'Marion Cotillard', 'Jack Madigan'],
     title: 'Macbeth',
   },
   {
     backgroundColor: '#92918b',
-    backgroundImg: 'https://9.react.pages.academy/static/film/background/Revenant.jpg',
-    description: 'A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team.',
+    backgroundImg:
+      'https://9.react.pages.academy/static/film/background/Revenant.jpg',
+    description:
+      'A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team.',
     director: 'Alejandro G. Iñárritu',
     duration: 156,
     genre: 'Action',
@@ -294,39 +193,36 @@ const films: CardType[] = [
     isInMyList: false,
     movieLink: 'https://9.react.pages.academy/static/film/video/matrix.mp4',
     poster: 'https://9.react.pages.academy/static/film/poster/Revenant.jpg',
-    previewImg: 'https://9.react.pages.academy/static/film/preview/revenant.jpg',
+    previewImg:
+      'https://9.react.pages.academy/static/film/preview/revenant.jpg',
     previewVideoLink: 'https://9.react.pages.academy/static/film/video/dog.mp4',
     rating: 4,
     releaseDate: 2015,
     scores: 618498,
-    starring: [
-      'Leonardo DiCaprio',
-      'Tom Hardy',
-      'Will Poulter',
-    ],
+    starring: ['Leonardo DiCaprio', 'Tom Hardy', 'Will Poulter'],
     title: 'The Revenant',
   },
   {
     backgroundColor: '#c6cadf',
-    backgroundImg: 'https://9.react.pages.academy/static/film/background/Seven_Years_in_Tibet.jpg',
-    description: 'True story of Heinrich Harrer, an Austrian mountain climber who became friends with the Dalai Lama at the time of China\'s takeover of Tibet.',
+    backgroundImg:
+      'https://9.react.pages.academy/static/film/background/Seven_Years_in_Tibet.jpg',
+    description:
+      'True story of Heinrich Harrer, an Austrian mountain climber who became friends with the Dalai Lama at the time of China\'s takeover of Tibet.',
     director: 'Jean-Jacques Annaud',
     duration: 136,
     genre: 'Adventure',
     id: 8,
     isInMyList: false,
     movieLink: 'https://9.react.pages.academy/static/film/video/bike.mp4',
-    poster: 'https://9.react.pages.academy/static/film/poster/Seven_Years_in_Tibet.jpg',
-    previewImg: 'https://9.react.pages.academy/static/film/preview/seven-years-in-tibet.jpg',
+    poster:
+      'https://9.react.pages.academy/static/film/poster/Seven_Years_in_Tibet.jpg',
+    previewImg:
+      'https://9.react.pages.academy/static/film/preview/seven-years-in-tibet.jpg',
     previewVideoLink: 'https://9.react.pages.academy/static/film/video/dog.mp4',
     rating: 3.6,
     releaseDate: 1997,
     scores: 112612,
-    starring: [
-      'Brad Pitt',
-      'David Thewlis',
-      'BD Wong',
-    ],
+    starring: ['Brad Pitt', 'David Thewlis', 'BD Wong'],
     title: 'Seven Years in Tibet',
   },
 ];
@@ -345,5 +241,6 @@ export type Review = {
 const MAXSCORE = 10;
 const DELAY = 1000;
 const FILMS_MAX_COUNT = 4;
+const TABS = ['Overview', 'Details', 'Reviews'];
 
-export {Genres,  PromoBudapest, films, MAXSCORE, DELAY, FILMS_MAX_COUNT };
+export { Genres, PromoBudapest, films, MAXSCORE, DELAY, FILMS_MAX_COUNT, TABS };
